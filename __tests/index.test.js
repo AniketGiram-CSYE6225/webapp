914bc9d318a1d6825377e032e0aafd1bdbb82cce
Aniketgiram <giram.a@northeastern.edu>
2024-02-07 19:12:10 -0500

    added extra command to start the test

--- __tests/index.test.js
+++ __tests/index.test.js
@@ -5,12 +5,11 @@ import db_conn from '../database/index.js'
 describe("GET /healthz", () => {
   it("responds with 200 if db connects", async () => {
     const response = await request(app).get("/healthz");
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(503);
   });
 });
 
 afterAll(done => {
-  // Closing the DB connection allows Jest to exit successfully.
   db_conn.close()
   done()
 })
