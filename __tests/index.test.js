3683671e3ffce51f9fb00bbd757d7ae41fb71c9a
Aniketgiram <giram.a@northeastern.edu>
2024-02-07 19:06:29 -0500

    checking workflow

--- __tests/index.test.js
+++ __tests/index.test.js
@@ -5,7 +5,7 @@ import db_conn from '../database/index.js'
 describe("GET /healthz", () => {
   it("responds with 200 if db connects", async () => {
     const response = await request(app).get("/healthz");
-    expect(response.statusCode).toBe(503);
+    expect(response.statusCode).toBe(200);
   });
 });
 
