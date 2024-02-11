eaff76aacccb81c7c871b19221ed88ad0749f1ad
Aniketgiram <giram.a@northeastern.edu>
2024-02-11 18:53:42 -0500

    sync db disabled to check

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import app from './index.js'
 import db_conn from "./database/index.js"
 
 app.listen(8080, async ()=>{
-    try {
-        console.log("connecting to db");
-        await db_conn.sync()
-    } catch (e) {
-        console.log("error in db syncing", e);
-        return e
-    }
+    // try {
+    //     console.log("connecting to db");
+    //     await db_conn.sync()
+    // } catch (e) {
+    //     console.log("error in db syncing", e);
+    //     return e
+    // }
 })
