959352aca3a15fee10fd0e1d975cc18ab0b1d923
Aniketgiram <giram.a@northeastern.edu>
2024-02-11 18:50:44 -0500

    typo fixed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import db_conn from "./database/index.js"
 
 app.listen(8080, async ()=>{
     try {
+        console.log("connecting to db");
         await db_conn.sync()
     } catch (e) {
         console.log("error in db syncing", e);
