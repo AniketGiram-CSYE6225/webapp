3ff9a2d9066466ecb23e1aff15e3941a10900aaf
Aniketgiram <giram.a@northeastern.edu>
2024-02-11 18:50:44 -0500

    typo fixed

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,11 +1,11 @@
 import supertest from 'supertest'
-import 'isomorphic-fetch'
+// import 'isomorphic-fetch'
 import app from '../index.js'
 const requestWithSupertest = supertest(app);
 
 describe("Get /healtz", () => {
     test("Should not connect to db", async () => {
-        const response = await requestWithSupertest.get("/healtz");
+        const response = await requestWithSupertest.get("/healthz");
         expect(response.statusCode).toBe(503)
     })
 })
