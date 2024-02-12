f331e4673488eb3ad2185ea001613f5daf328de0
Aniketgiram <giram.a@northeastern.edu>
2024-02-11 19:36:19 -0500

    added secreat to github and configured in action

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,11 +1,10 @@
 import supertest from 'supertest'
-// import 'isomorphic-fetch'
 import app from '../index.js'
 const requestWithSupertest = supertest(app);
 
 describe("Get /healtz", () => {
     test("Should not connect to db", async () => {
         const response = await requestWithSupertest.get("/healthz");
-        expect(response.statusCode).toBe(503)
+        expect(response.statusCode).toBe(200)
     })
 })
--- server.js
+++ server.js
@@ -1,12 +1,3 @@
 import app from './index.js'
-import db_conn from "./database/index.js"
 
-app.listen(8080, async ()=>{
-    try {
-        console.log("connecting to db");
-        await db_conn.sync()
-    } catch (e) {
-        console.log("error in db syncing", e);
-        return e
-    }
-})
+app.listen(8080)
