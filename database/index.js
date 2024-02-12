f410039ea9c8527b1c94d83ca7d16201f920ddd6
Aniketgiram <giram.a@northeastern.edu>
2024-02-11 19:50:35 -0500

    checked to dynamic port

--- database/index.js
+++ database/index.js
@@ -10,8 +10,7 @@ function createConnection() {
             dialect: process.env.DIALECT,
             database: process.env.DB_NAME,
             username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            port: process.env.PORT
+            password: process.env.DB_PASSWORD
         })
     } catch (error) {
         return error
