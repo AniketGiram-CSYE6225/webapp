28572d485be8bec0cd07d44b8f218a0bb9dbb346
Aniketgiram <giram.a@northeastern.edu>
2024-02-11 19:38:49 -0500

    checking testcase

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import healthzRoute from './healthz.js'
-import userRoute from './user.js'
+// import userRoute from './user.js'
 const router = express.Router()
 
 router.use("/healthz", healthzRoute)
-router.use("/v1/user", userRoute)
+// router.use("/v1/user", userRoute)
 
 export default router
