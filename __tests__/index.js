f5a79044a42051cd179a004ca634cad9006f82c8
Aniketgiram <giram.a@northeastern.edu>
2024-02-11 22:04:00 -0500

    added user routes and db connection check test

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,7 +3,7 @@ import app from '../index.js'
 const requestWithSupertest = supertest(app);
 
 describe("Get /healtz", () => {
-    test("Should not connect to db", async () => {
+    test("connect to db", async () => {
         const response = await requestWithSupertest.get("/healthz");
         expect(response.statusCode).toBe(200)
     })
