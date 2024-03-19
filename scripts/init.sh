3f87befcb7c4d76d9acc32cf3fa7db9d0dcfca0e
Aniketgiram <giram.a@northeastern.edu>
2024-03-19 14:19:02 -0400

    code restructured

--- scripts/init.sh
+++ scripts/init.sh
@@ -30,15 +30,4 @@ cd /opt/webapp
 
 sudo npm install
 
-# installing Ops Agent
-cd /var/log
-
-sudo mkdir webapp
-
-cd webapp
-
-sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-
-sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-
 echo "completed all steps in init.sh"
