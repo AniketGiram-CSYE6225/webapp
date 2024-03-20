441e344e94414f73683935d1767169a1ed38a2c4
Aniketgiram <giram.a@northeastern.edu>
2024-03-20 14:48:55 -0400

    logger code updated

--- logger/index.js
+++ logger/index.js
@@ -1,8 +1,23 @@
 import winston from "winston"
+const { createLogger, format } = winston;
+// import {LoggingWinston} from "@google-cloud/logging-winston";
 
-export const logger = winston.createLogger({
-    transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({ filename: '/var/log/webapp/nscc-webapp.log' })
-    ]
-  });
+// const loggingWinston = new LoggingWinston();
+
+export const logger = createLogger({
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(({ level, message, timestamp }) => {
+      return JSON.stringify({
+        severity: level.toUpperCase(),
+        message,
+        timestamp
+      });
+    })
+  ),
+  transports: [
+    new winston.transports.File({ filename: '/var/log/webapp/nscc-webapp.log' })
+  ]
+});
+
+// loggingWinston,
--- routes/healthz.js
+++ routes/healthz.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import db_conn from '../database/index.js'
 const router = express.Router()
+import {logger} from './logger/index.js'
 
 router.get("/", async (request, response) => {
     try {
         await db_conn.authenticate()
+        logger.info("Health is Good")
         return response.status(200).send()
     } catch (e) {
+        logger.error(`error ${e}`)
         return response.status(503).send()
     }
 })
