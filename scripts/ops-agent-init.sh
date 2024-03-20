223f5938a167045b4917ee51a42c82e958e96a32
Aniketgiram <giram.a@northeastern.edu>
2024-03-19 20:46:15 -0400

    added logging

--- scripts/ops-agent-init.sh
+++ scripts/ops-agent-init.sh
@@ -11,4 +11,9 @@ sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo
 
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
+# move logging yml
+sudo mv /opt/webapp/config.yaml /etc/google-cloud-ops-agent/config.yaml
+
+sudo systemctl restart google-cloud-ops-agent
+
 echo "completed all steps in ops-agent-init.sh"
