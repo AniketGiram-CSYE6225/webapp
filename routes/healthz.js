a1aad4e48e8b893062d3e02fdbc7482f16f1d94a
Aniketgiram <giram.a@northeastern.edu>
2024-03-20 16:40:06 -0400

    logging added in controllers

--- routes/healthz.js
+++ routes/healthz.js
@@ -7,9 +7,11 @@ router.get("/", async (request, response) => {
     try {
         await db_conn.authenticate()
         logger.info("Health is Good")
+        logger.debug("Application is healthy")
         return response.status(200).send()
     } catch (e) {
         logger.error(`error ${e}`)
+        logger.debug("Application is not healthy")
         return response.status(503).send()
     }
 })
